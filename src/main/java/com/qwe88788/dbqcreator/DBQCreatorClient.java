8d50fecaf28d9d7754c354b1124154003254e920
qwe88 <qwe88788@outlook.com>
2023-12-24 16:49:11 +0800

    0.1-完善基础架构

---
+++ src/main/java/com/qwe88788/dbqcreator/DBQCreatorClient.java
@@ -0,0 +1,11 @@
+package com.qwe88788.dbqcreator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DBQCreatorClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/qwe88788/dbqcreator/DBQCreatorDataGenerator.java
@@ -0,0 +1,11 @@
+package com.qwe88788.dbqcreator;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DBQCreatorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
